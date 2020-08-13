8378f3076176fa756836cc1cd573e48e4f96b637
pattybee <66446574+pattybee@users.noreply.github.com>
2020-08-14 08:08:51 +1000

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,8 +1,13 @@
+  
 setwd("C:/Users/patty/OneDrive/Desktop/Coursera/Exploratry_data_analysis")
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = "FNEI_data.csv")
 library(dplyr)
 library(bindrcpp)
 library(ggplot2)
+
+# Load the NEI & SCC data frames.
+NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
+SCC <- data.table::as.data.table(x = readRDS("Source_Classification_Code.rds"))
 
 # forming Baltimore data which will be NEI_data subset
 baltdata <- NEI_data[NEI_data$fips=="24510", ]
